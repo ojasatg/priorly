9bb15888ce6f985af567bf40cefb426db288fce1
Ojaswi Athghara <ojaswi@acalvio.com>
2024-07-29 07:52:47 +0530

    set up server and mongo db

---
+++ packages/server/src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3010;
+const MONGO_URI = String(process.env.MONGO_URI);
+
+app.get("/test", (_req, res) => {
+    res.send("Test successful!");
+});
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.info("MongoDB connected");
+        app.listen(PORT, () => {
+            console.info(`Server started: Listening on PORT: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error(error);
+        console.error(
+            "Error connecting to MongoDB. Please check your connection string in .env file.",
+        );
+    });
